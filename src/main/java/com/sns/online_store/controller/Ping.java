9bff5d7ac9a48987cec6135e9e4572c901897690
Shakalyan <vdovit@mail.ru>
2023-04-02 21:46:09 +0300

    Initial commit 2

--- src/main/java/com/sns/online_store/controller/Ping.java
+++ src/main/java/com/sns/online_store/controller/Ping.java
@@ -1,2 +1,14 @@
-package com.sns.online_store.controller;public class Ping {
+package com.sns.online_store.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Ping {
+
+    @GetMapping("/")
+    public String ping() {
+        return "pong";
+    }
+
 }
