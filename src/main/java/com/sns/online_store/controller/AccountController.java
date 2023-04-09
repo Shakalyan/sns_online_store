b5b560753058fd42a572fbb24407ef6abe5103b2
Shakalyan <vdovit@mail.ru>
2023-04-09 16:01:17 +0300

    Registration controller fix

--- src/main/java/com/sns/online_store/controller/AccountController.java
+++ src/main/java/com/sns/online_store/controller/AccountController.java
@@ -4,19 +4,16 @@ import com.sns.online_store.dto.RegistrationRequest;
 import com.sns.online_store.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/auth")
 public class AccountController {
 
     private final AccountService accountService;
 
-    @GetMapping("/registration")
+    @PostMapping("/registration")
     public ResponseEntity<String> register(@RequestBody RegistrationRequest request) {
         accountService.register(request);
         return ResponseEntity.ok("ok");
